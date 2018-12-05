a3cf188625af8e64df719ce5b7b67c0104b2ff4d
杨盼 <uniquevister@126.com>
2018-12-05 21:35:29 +0800

    更新app.vue文件

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,34 @@
 <template>
-	<div class="app-container">
-    <!-- 顶部 Header 区域 -->
-  <mt-header fixed title="测 试"></mt-header>
+	<div>
+    <!-- 中间的 路由 router-view 区域 -->
+	<transition>
+		<router-view></router-view>
+	</transition>
 
-      <!-- 中间的 路由 router-view 区域 -->
-		<transition>
-			<router-view></router-view>
-		</transition>
-</div>
+    <!-- tabbar -->
+  <nav class="mui-bar mui-bar-tab mui-active">
+    <router-link class="mui-tab-item" to="/home">
+      <span class="mui-icon mui-icon-home"></span>
+      <span class="mui-tab-label">首页</span>
+    </router-link>
+    <router-link class="mui-tab-item" to="/video">
+      <span class="mui-icon mui-icon-videocam"></span>
+      <span class="mui-tab-label">视频</span>
+    </router-link>
+    <router-link class="mui-tab-item" to="/search">
+      <span class="mui-icon mui-icon-search"></span>
+      <span class="mui-tab-label">发现</span>
+    </router-link>
+    <router-link class="mui-tab-item" to="/message">
+      <span class="mui-icon mui-icon-email"><span class="mui-badge">20</span></span>
+      <span class="mui-tab-label">消息</span>
+    </router-link>
+    <router-link class="mui-tab-item" to="/member">
+      <span class="mui-icon mui-icon-contact"></span>
+      <span class="mui-tab-label">我的</span>
+    </router-link>
+  </nav>
+  </div>
 </template>
 
 <script>
@@ -15,10 +36,21 @@
 </script>
 
 <style lang="scss" scoped>
-    .app-container {
-    padding: 20px;
-    overflow-x: hidden;
-  }
+  // .v-enter {
+  //   opacity: 0;
+  //   transform: translateX(100%);
+  // }
+
+  // .v-leave-to {
+  //   opacity: 0;
+  //   transform: translateX(-100%);
+  //   position: absolute;
+  // }
+
+  // .v-enter-active,
+  // .v-leave-active {
+  //   transition: all 0.5s ease;
+  // }
 
 
 </style>
